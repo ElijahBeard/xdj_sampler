6f10631426d27c312456c363b051040a312bbfa8
ElijahBeard <elijahbeard24@gmail.com>
2025-08-25 16:08:23 -0400

    sequencer

--- sequencer.h
+++ sequencer.h
@@ -2,21 +2,23 @@
 #include <SDL2/SDL.h>
 #include "util.h"
 
+#define _STEPS_ 8
+
 SDL_Surface* step_on;
 SDL_Surface* step_off;
-SDL_Surface* steps[16];
+SDL_Surface* steps[_STEPS_];
 
 void load_sequencer() { 
     step_on = SDL_LoadBMP("assets/sequencer/1.bmp");
     step_off = SDL_LoadBMP("assets/sequencer/0.bmp");
-    for(int i = 0; i < 16; i++) {
+    for(int i = 0; i < _STEPS_; i++) {
         steps[i] = step_off;
     }
 }
 
 void render_sequencer() {
-    SDL_Rect step_rect = {step_on->h,step_on->w,100,100};
-    for(int i = 0; i < 16; i++) {
+    SDL_Rect step_rect = {step_on->h,step_on->w,0,0};
+    for(int i = 0; i < _STEPS_; i++) {
         SDL_BlitSurface(steps[i],NULL,win_surface,&step_rect);
         step_rect.x = step_rect.x + 10;
         if(i==7){step_rect.y = step_rect.y + 10; step_rect.x = step_rect.x - 60;}
@@ -25,7 +27,7 @@ void render_sequencer() {
 }
 
 void free_sequencer() {
-    for(int i = 0; i < 16; i++) {
+    for(int i = 0; i < _STEPS_; i++) {
         SDL_FreeSurface(steps[i]);
     }
 }
